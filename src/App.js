f46c701b5ebf1ae8c66d1c98dc212c820390d02d
Durieu Emilien <contact@emilienduri.eu>
2021-02-27 22:32:25 +0100

    do the thing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [svgString, setSvgString] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [colors, setColors] = useState([])
+  useEffect(() => {
+    if (!svgString && !loading) {
+      fetch('/flemcoaster.svg')
+        .then((res) => res.text())
+        .then((svg) => {
+          setSvgString(svg)
+          setLoading(false)
+        })
+      setLoading(true)
+    }
+  }, [svgString, loading])
+  const paths = document.querySelectorAll('#svgContainer path')
+  useEffect(() => {
+    if (paths && colors.length) {
+      colors.forEach((c, i) => {
+        paths[i].setAttribute('fill', c)
+      })
+
+      const canvas = document.querySelector('canvas')
+      const data = new XMLSerializer().serializeToString(
+        document.querySelector('#svgContainer svg')
+      )
+      const win = window.URL || window.webkitURL || window
+      const img = new Image()
+
+      const blob = new Blob([data], { type: 'image/svg+xml' })
+      const url = win.createObjectURL(blob)
+      img.setAttribute('src', url)
+      img.addEventListener('load', () => {
+        canvas.getContext('2d').drawImage(img, 0, 0)
+        win.revokeObjectURL(url)
+        const uri = canvas
+          .toDataURL('image/png')
+          .replace('image/png', 'octet/stream')
+        const a = document.querySelector('#download')
+        a.setAttribute('href', uri)
+        a.setAttribute('download', 'flemcoaster.png')
+      })
+      //return () => window.URL.revokeObjectURL(uri)
+    } else if (!colors.length) {
+      setColors(
+        Array.from(paths).map((p) => p.getAttribute('fill') || '#000000')
+      )
+    }
+  }, [colors, paths])
+  console.log(colors)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{ opacity: 0, pointerEvents: 'none', position: 'absolute' }}
+        id="svgContainer"
+        dangerouslySetInnerHTML={{ __html: svgString }}
+      ></div>
+      <canvas width="1116.606" height="612.147"></canvas>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+        }}
+      >
+        {colors.length &&
+          colors.map((p, i) => {
+            const color = p && p.length === 4 ? p + p[3] + p[3] + p[3] : p
+            return (
+              <input
+                type="color"
+                onChange={(ev) => {
+                  const newColors = [...colors]
+                  newColors[i] = ev.target.value
+                  setColors(newColors)
+                }}
+                value={color}
+              />
+            )
+          })}
+        <a id="download">Télécharger</a>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
